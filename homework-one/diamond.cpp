6f29f19535e729d3dcc886c3309db3cdf8d44778
maarnold7 <maarnold7@alaska.edu>
2019-09-09 09:16:25 -0800

    Top of the diamond complete

--- homework-one/diamond.cpp
+++ homework-one/diamond.cpp
@@ -3,21 +3,24 @@
 void main()
 {
 	int rows;
+	std::cout << "Enter a number for the diamond\n";
 	std::cin >> rows;
-	int currow = 0;
-	int rowchars = rows * 2 + 1;
-	int offset = 1;
-	while (currow < rows)
+	int currow = 1;
+	while (currow <= rows)
 	{
-		currow++;
-		for (int i = 0; i < rowchars / 2 - offset; i++)
+		for (int i = currow; i < rows; i++)
 		{
-			std::cout << i;
+			std::cout << " ";
 		}
-		for (int i = 0; i < offset; i++)
+		for (int i = 1; i < 2*currow; i++)
 		{
-			std::cout << "b";
+			std::cout << "#";
+		}
+		for (int i = currow; i < rows; i++)
+		{
+			std::cout << " ";
 		}
 		std::cout << "\n";
+		currow++;
 	}
 }
