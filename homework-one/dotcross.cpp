61d58cc5c6c29ac42a90e1ed43e5c1bb71d0935a
maarnold7 <maarnold7@alaska.edu>
2019-09-13 09:40:58 -0800

    Created dotcross & working on it

---
+++ homework-one/dotcross.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+void main()
+{
+	int[2][3]
+	for (int i = 1; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			std::cout << "For vector " << i << ", input the vector value at index: " << j;
+		}
+	}
+}
