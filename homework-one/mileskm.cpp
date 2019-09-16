4968cb5ff259a67a5e7a71368f600a2ae19c3d2c
Millard Arnold V <millard.arnold.v@gmail.com>
2019-09-15 18:11:04 -0800

    mileskm working properly

--- homework-one/mileskm.cpp
+++ homework-one/mileskm.cpp
@@ -2,5 +2,8 @@
 
 void main() 
 {
-	std::cout << "hello";
+	float miles;
+	std::cout << "Please enter the number of miles you would like to convert to kilometers. ";
+	std::cin >> miles;
+	std::cout << miles << " miles is " << miles * 1.609 << " kilometers";
 }
