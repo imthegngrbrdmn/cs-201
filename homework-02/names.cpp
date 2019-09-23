a4f29adbd6c6db3ad161f365424c25116c785a80
Millard Arnold V <millard.arnold.v@gmail.com>
2019-09-22 18:13:43 -0800

    DoesNameExist exists as a functional function

--- homework-02/names.cpp
+++ homework-02/names.cpp
@@ -2,20 +2,37 @@
 #include <vector>
 #include <string>
 #include <algorithm>
-void inputNames(std::vector<std::string> &names);
+void InputNames(std::vector<std::string>& names);
+bool DoesNameExist(const std::string& nameToFind, const std::vector<std::string>& names);
+
 int main(int argc, char** argv)
 {
 	std::vector<std::string> names;
-	inputNames(names);
+	InputNames(names);
+	std::cout << "Please enter a name to find: ";
+	std::string nameToFind;
+	std::cin >> nameToFind;
+	std::cout << "Name is in list: " << DoesNameExist(nameToFind, names);
 	return 0;
 }
-void inputNames(std::vector<std::string> &names)
+void InputNames(std::vector<std::string> &names)
 {
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < 4/*10*/; i++)//using 4 for testing
 	{
 		std::string name;
 		std::cout << "Please enter a name: ";
 		std::getline(std::cin, name);
 		names.push_back(name);
 	}
+}
+bool DoesNameExist(const std::string& nameToFind, const std::vector<std::string>& names)
+{
+	for (std::string name : names)
+	{
+		if (name == nameToFind)
+		{
+			return true;
+		}
+	}
+	return false;
 }
