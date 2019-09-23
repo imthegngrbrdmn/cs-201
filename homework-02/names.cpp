520dc27a075145feb90a114320da79abdb0dcf37
Millard Arnold V <millard.arnold.v@gmail.com>
2019-09-22 18:28:41 -0800

    Created ZanyNames -Untested

--- homework-02/names.cpp
+++ homework-02/names.cpp
@@ -5,6 +5,7 @@
 bool DoesNameExist(const std::string& nameToFind, const std::vector<std::string>& names);
 void InputNames(std::vector<std::string>& names);
 void PrintNames(const std::vector<std::string>& names);
+void ZanyNames(const std::vector<std::string>& names);
 
 int main(int argc, char** argv)
 {
@@ -17,18 +18,9 @@ int main(int argc, char** argv)
 	std::cout << "Name is in list: " << DoesNameExist(nameToFind, names)<<"\n";
 	std::cout << "Printing names: \n";
 	PrintNames(names);
+	std::cout << "Printing names with the first half at the end: \n";
+	ZanyNames(names);
 	return 0;
-}
-
-void InputNames(std::vector<std::string>& names)
-{
-	for (int i = 0; i < 4/*10*/; i++)//using 4 for testing
-	{
-		std::string name;
-		std::cout << "Please enter a name: ";
-		std::getline(std::cin, name);
-		names.push_back(name);
-	}
 }
 
 bool DoesNameExist(const std::string& nameToFind, const std::vector<std::string>& names)
@@ -43,10 +35,28 @@ bool DoesNameExist(const std::string& nameToFind, const std::vector<std::string>
 	return false;
 }
 
+void InputNames(std::vector<std::string>& names)
+{
+	for (int i = 0; i < 4/*10*/; i++)//using 4 for testing
+	{
+		std::string name;
+		std::cout << "Please enter a name: ";
+		std::getline(std::cin, name);
+		names.push_back(name);
+	}
+}
+
 void PrintNames(const std::vector<std::string>& names)
 {
 	for (std::string name : names)
 	{
 		std::cout << name << "\n";
+	}
+}
+void ZanyNames(const std::vector<std::string>& names)
+{
+	for (std::string name : names)
+	{
+		std::cout << name.substr(name.length()/2) << name.substr(0,name.length()/2) << "\n";
 	}
 }
