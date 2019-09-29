c1e856ba723a3a06afa44ffba80d4e86b8adfed4
Millard Arnold V <millard.arnold.v@gmail.com>
2019-09-29 13:02:38 -0800

    Added input

--- homework-03/boxprint.cpp
+++ homework-03/boxprint.cpp
@@ -4,6 +4,12 @@
 
 int main()
 {
-	printBox("Hello, World", 5);
+	std::string message;
+	int layers;
+	std::cout << "What would you like to say? ";
+	std::cin >> message;
+	std::cout << "How many layers would you like? ";
+	std::cin >> layers;
+	printBox(message, layers);
 	return 0;
 }
