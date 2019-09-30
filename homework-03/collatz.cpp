b5e772d98c42d027d5b0348e48e401dbacebf015
Millard A. Arnold V <maarnold7@alaska.edu>
2019-09-29 20:15:42 -0800

    Finished the collatz program (untested)

--- homework-03/collatz.cpp
+++ homework-03/collatz.cpp
@@ -1,3 +1,26 @@
 #include<iostream>
 #include<vector>
-
+int collatz();
+int main()
+{
+	int a;
+	std::cout<<"Insert a number to begin: ";
+	std::cin>>a;	
+	while(a>1)
+		{
+			std::cout<<a<<"\t";
+			a=collatz(a);
+		}
+	return 0;
+}
+int collatz(int num)
+{
+	if(num%2==0)
+	{
+		return num/2;
+	}
+	else
+	{
+		return num*3+1;
+	}
+}
