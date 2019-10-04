938cdc305a720db7c7806da6be542e01efd09a8b
maarnold7 <maarnold7@alaska.edu>
2019-10-04 10:12:07 -0800

    Lab_14

---
+++ Lab_14/lab14.cpp
@@ -0,0 +1,16 @@
+#include"lab14.h"
+void modStr(std::string str)
+{
+	for (int i = str.length(); i > 0; i--)
+	{
+		std::cout << str.substr(0, i) << "\n";
+	}
+}
+void modStrBoth(std::string str)
+{
+	
+	for (int i = 0; i < (int) str.length()/2; i++)
+	{
+		std::cout << str.substr(i, str.length()-2*i) << "\n";
+	}
+}
---
+++ Lab_14/main.cpp
@@ -0,0 +1,9 @@
+#include"lab14.h"
+int main()
+{
+	std::string str;
+	std::cout << "Input a string: ";
+	std::cin >> str;
+	modStrBoth(str);
+	return 0;
+}
