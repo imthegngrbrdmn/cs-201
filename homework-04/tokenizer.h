2dc808d61f98442937332c7e355e0f7317a8b3c9
Millard Arnold V <millard.arnold.v@gmail.com>
2019-10-13 18:39:21 -0800

    Homework-04, Created tokenizer.h

---
+++ homework-04/tokenizer.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include <vector>
+bool ReadLine(std::string& str);
+unsigned StringToTokenWS(std::vector<std::string>& tokens);
+void AnalyseTokens(const std::vector<std::string>& tokens);
