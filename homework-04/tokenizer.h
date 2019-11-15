0e00f1a463eddef7603be9201d4b760b2436be5c
Millard A. Arnold V <maarnold7@alaska.edu>
2019-11-14 16:42:47 -0900

    stuff

--- homework-04/tokenizer.h
+++ homework-04/tokenizer.h
@@ -2,6 +2,21 @@
 #include <string>
 #include <iostream>
 #include <vector>
+
+/**
+Uses std::getline() to get a line of text from the user. 
+Returns true if it read the string & false if the string was a blank line.
+*/
 bool ReadLine(std::string& str);
+
+/**
+Uses std::istringstream to read strings separated by whitespace characters.
+Push a blank string at the end of each line. 
+Returns the number of tokens read from the string.
+*/
 unsigned StringToTokenWS(std::vector<std::string>& tokens);
+
+/**
+prints out the type of token and the token itself surrounded by quotation marks.
+*/
 void AnalyseTokens(const std::vector<std::string>& tokens);
