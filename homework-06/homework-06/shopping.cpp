35f5d216d86726e66036c162bd7b2a23ba217164
Millard Arnold V <millard.arnold.v@gmail.com>
2019-11-17 19:08:03 -0900

    can do stuff now

--- homework-06/homework-06/shopping.cpp
+++ homework-06/homework-06/shopping.cpp
@@ -1,15 +1,67 @@
 #include "shopping.h"
 int main()
 {
+	std::string action;
 	std::map<std::string, Record> inventory;
 	inventory["apple"] = {0.75,7};
 	inventory["pear"] = { 1.5,4 };
 	inventory["banana"] = { 0.25,20 };
 	inventory["pineapple"] = { 2.5,2 };
 	inventory["orange"] = { 0.1,42 };
+	double total=0;
+	printInventory(inventory);
+	std::cout << "You can 'see stuff', 'buy', 'leave', or 'remove item'.\n";
+	while (true)
+	{
+		getline(std::cin,action);
+		std::transform(action.begin(), action.end(), action.begin(), ::tolower);
+		if (action == "see stuff")
+		{
+			printInventory(inventory);
+		} 
+		else if (action == "buy")
+		{
+			std::cout<<"What would you like to buy? ";
+			std::string item;
+			getline(std::cin, item);
+			buy(item,inventory,total);
+		}
+		else if (action == "leave")
+		{
+			break;
+		}
+		else if (action == "remove item")
+		{
+			std::cout << "What would you like to remove? ";
+			std::string item;
+			getline(std::cin, item);
+			drop(item, inventory, total);
+		}
+		std::cout << "Current total is " << total<<"\n";
+	}
+	return 0;
+}
+void printInventory(std::map<std::string, Record>& inventory)
+{
 	for (auto item : inventory)
 	{
 		std::cout << "We have " << item.second.units << " " << item.first << "s for the low price of " << item.second.unitPrice << "\n";
 	}
-	return 0;
+}
+void buy(std::string item, std::map<std::string, Record>& inventory, double& total)
+{
+	if (inventory[item].units > 0)
+	{
+		inventory[item].units -= 1;
+		total += inventory[item].unitPrice;
+	}
+	else
+	{
+		std::cout << "We don't have more of that.";
+	}
+}
+void drop(std::string item, std::map<std::string, Record>& inventory, double& total)
+{
+	inventory[item].units += 1;
+	total -= inventory[item].unitPrice;
 }
--- homework-06/homework-06/shopping.h
+++ homework-06/homework-06/shopping.h
@@ -3,9 +3,13 @@
 #include <string>
 #include <map>
 #include <cstdlib>
+#include <algorithm> 
 
 struct Record
 {
 	double unitPrice;
 	int units;
 };
+void printInventory(std::map<std::string, Record>& inventory);
+void buy(std::string item, std::map<std::string, Record>& inventory, double& total);
+void drop(std::string item, std::map<std::string, Record>& inventory, double& total);
